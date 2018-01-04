4d316251d20e1b560adc307e215f1ba4b7a64bb3
Richard Ogamba <ogambarichard20@gmail.com>
2018-01-04 19:32:24 +0300

    add a date custom pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,12 +17,12 @@ export class AppComponent {
   //
   // ]
   goals = [
-    new Goal(1, 'Watch Finding Nemo', 'Find an online version and watch merlin find his son'),
-    new Goal(2, 'Buying Cookies', 'I have to buy cookies for the parrot'),
-    new Goal(3, 'Get New Phone Case', 'Diana has her birthday coming up soon'),
-    new Goal(4, 'Get Dog Food', 'Pupper likes expesive snacks'),
-    new Goal(5, 'Solve Math Homework', 'Damn math'),
-    new Goal(6, 'Plot My World Domination Plan', 'Cause I am an evil Overlord'),
+    new Goal(1, 'Watch Finding Nemo', 'Find an online version and watch merlin find his son', new Date(2018,3,14) ),
+    new Goal(2, 'Buying Cookies', 'I have to buy cookies for the parrot', new Date(2018,6,9) ),
+    new Goal(3, 'Get New Phone Case', 'Diana has her birthday coming up soon', new Date(2018,1,12) ),
+    new Goal(4, 'Get Dog Food', 'Pupper likes expesive snacks', new Date(2018,0,18) ),
+    new Goal(5, 'Solve Math Homework', 'Damn math', new Date(2081,2,14) ),
+    new Goal(6, 'Plot My World Domination Plan', 'Cause I am an evil Overlord', new Date(2018,3,14) ),
 
   ]
   // constructor(){}
--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,5 +1,7 @@
 export class Goal {
 	public showDescription:boolean;
-	constructor(public id:number, public name:string, public description:string){this.showDescription =  false}
+	constructor(public id:number, public name:string, public description:string, public completeData:Date){
+		this.showDescription =  false
+	}
 
 }
