5128347d99940e919c214c24d81f90555531f67d
Richard Ogamba <ogambarichard20@gmail.com>
2018-01-04 12:57:56 +0300

    create classes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {Goal} from './goal'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
-  constructor() { }
+  goals = [
+    new Goal(1, 'Watch Finding Nemo'),
+    new Goal(2, 'Buying Cookies'),
+    new Goal(3, 'Get New Phone Case'),
+    new Goal(4, 'Get Dog Food'),
+    new Goal(5, 'Solve Math Homework'),
+    new Goal(6, 'Plot My World Domination Plan'),
 
-  this.goals = ["Watch Find Nemo", "Buy Cookies", "Get New Phone Case"]
+  ]
 }
