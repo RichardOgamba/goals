a1e9f6dac5d3554f7942b8f6358974240c2d63be
Richard Ogamba <ogambarichard20@gmail.com>
2018-01-11 10:02:55 +0300

    link the service component

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Goal} from '../goal'
+import {Goals} from '../goals'
 
 @Component({
   selector: 'app-goal',
@@ -7,16 +8,17 @@ import {Goal} from '../goal'
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
+  goals = Goals;
 
-  goals = [
-    new Goal(1, 'Watch Finding Nemo', 'Find an online version and watch merlin find his son', new Date(2018,3,14) ),
-    new Goal(2, 'Buying Cookies', 'I have to buy cookies for the parrot', new Date(2018,6,9) ),
-    new Goal(3, 'Get New Phone Case', 'Diana has her birthday coming up soon', new Date(2018,1,12) ),
-    new Goal(4, 'Get Dog Food', 'Pupper likes expesive snacks', new Date(2018,0,18) ),
-    new Goal(5, 'Solve Math Homework', 'Damn math', new Date(2081,2,14) ),
-    new Goal(6, 'Plot My World Domination Plan', 'Cause I am an evil Overlord', new Date(2018,3,14) ),
-
-  ]
+  // goals = [
+  //   new Goal(1, 'Watch Finding Nemo', 'Find an online version and watch merlin find his son', new Date(2018,3,14) ),
+  //   new Goal(2, 'Buying Cookies', 'I have to buy cookies for the parrot', new Date(2018,6,9) ),
+  //   new Goal(3, 'Get New Phone Case', 'Diana has her birthday coming up soon', new Date(2018,1,12) ),
+  //   new Goal(4, 'Get Dog Food', 'Pupper likes expesive snacks', new Date(2018,0,18) ),
+  //   new Goal(5, 'Solve Math Homework', 'Damn math', new Date(2081,2,14) ),
+  //   new Goal(6, 'Plot My World Domination Plan', 'Cause I am an evil Overlord', new Date(2018,3,14) ),
+  //
+  // ]
 
   deleteGoal(isComplete,index) {
     if(isComplete) {
